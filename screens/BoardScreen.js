55037decd0cadf18721fe4edfb2f941db7377e26
Jiyoung Yoon <56244911+ebbuni1023@users.noreply.github.com>
2021-07-03 02:52:17 -0500

    send to board firebase

--- screens/BoardScreen.js
+++ screens/BoardScreen.js
@@ -1,13 +1,19 @@
 import React, { useLayoutEffect, useState, useEffect } from 'react'
 import { TouchableOpacity } from 'react-native'
-import { StyleSheet, Text, View, StatusBar } from 'react-native'
+import { StyleSheet, Text, View, StatusBar, ScrollView, TextInput } from 'react-native'
 import { Avatar } from 'react-native-elements'
 import { AntDesign, FontAwesome, Ionicons  } from '@expo/vector-icons'
 import { SafeAreaView } from 'react-native'
 import { KeyboardAvoidingView } from 'react-native'
-import { Platform } from 'react-native'
+import { Platform, TouchableWithoutFeedback } from 'react-native'
+import { Keyboard } from 'react-native'
+import { db, auth } from '../firebase'
+import firebase from 'firebase'
+
 const BoardScreen = ({ navigation, route }) => {
 
+    // type message and send //
+    const [input, setInput] = useState("");
     useLayoutEffect(() => {
         navigation.setOptions({
             title:"Board",
@@ -43,6 +49,22 @@ const BoardScreen = ({ navigation, route }) => {
         });
 
     }, [navigation])
+
+    // SEND MESSAGE //
+    const sendMessage=() => {
+        Keyboard.dismiss();
+
+        db.collection('Boards').doc(route.params.id).collection('messages').add({
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            message: input,
+            displayName: auth.currentUser.displayName,
+            email: auth.currentUser.email,
+            photoURL: auth.currentUser.photoURL
+        })
+
+        setInput('')
+    };
+
     return (
         <SafeAreaView style={{flex: 1, backgroundColor: 'white' }}>
             <StatusBar style="dark"/>
@@ -50,13 +72,31 @@ const BoardScreen = ({ navigation, route }) => {
             behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
             style={styles.container}
             keyboardVerticalOffset={90}
-            />
-
+            >
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
             <>
                 <ScrollView>
                     {/* CHAT */}
                 </ScrollView>
+
+                <View style={styles.footer}>
+                    <TextInput
+                    value={input}
+                    onChangeText={(text) => setInput(text)}
+                    onSubmitEditing={sendMessage}
+                    placeholder='Say something for friends' 
+                    style={styles.textInput}
+                    />
+                    <TouchableOpacity
+                    onPress={sendMessage}
+                    activeOpacity={0.5}
+                    >
+                    <Ionicons name="send" size={24} color="purple"/>
+                </TouchableOpacity>
+                </View>
             </>
+            </TouchableWithoutFeedback>
+            </KeyboardAvoidingView>
             {/* <Text>{route.params.boardName}</Text> */}
         </SafeAreaView>
     )
@@ -64,4 +104,27 @@ const BoardScreen = ({ navigation, route }) => {
 
 export default BoardScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    footer:{
+        flexDirection: 'row',
+        padding: 15,
+        alignItems: 'center',
+        width: '100%',
+    },
+
+    textInput:{
+        bottom: 0,
+        height: 40,
+        flex: 1,
+        marginRight: 15,
+        borderColor: 'transparent',
+        backgroundColor: '#ECECEC',
+        // borderWidth: 1,
+        padding: 10,
+        color: 'grey',
+        borderRadius: 30,
+    }
+})
